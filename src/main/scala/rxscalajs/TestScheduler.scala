f55ab2f538c49f4b3c5d452c1066b6254be98aa3
a <4184070+MrCurtis@users.noreply.github.com>
2018-01-18 21:20:02 +0000

    Add WithObservableToBeFunction

--- src/main/scala/rxscalajs/TestScheduler.scala
+++ src/main/scala/rxscalajs/TestScheduler.scala
@@ -9,6 +9,11 @@ import rxscalajs.facade.ObservableFacade
 
 
 @js.native
+trait WithObservableToBeFunction extends js.Object {
+  def toBe(marbles: String): Unit
+}
+
+@js.native
 @JSGlobal("Rx.HotObservable")
 class HotObservable extends js.Object {
   def subscribe[T](observerOrNext: T=>Unit): Subscription = js.native
@@ -19,4 +24,6 @@ class HotObservable extends js.Object {
 class TestScheduler extends js.Object {
   def createHotObservable[T](marbles: T): ObservableFacade[T] = js.native
   def createColdObservable[T](marbles: T): ObservableFacade[T] = js.native
+  def expectObservable[T](observable: Observable[T]): WithObservableToBeFunction = js.native
 }
+
