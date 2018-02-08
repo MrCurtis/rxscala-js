3ad8a422bd417780f562bb3facb27e9251aa308d
a <4184070+MrCurtis@users.noreply.github.com>
2018-02-08 21:51:05 +0000

    More WIP

--- src/main/scala/rxscalajs/TestScheduler.scala
+++ src/main/scala/rxscalajs/TestScheduler.scala
@@ -10,14 +10,14 @@ import rxscalajs.facade.{ObservableFacade, SubjectFacade, HotObservableFacade}
 
 @js.native
 trait WithObservableToBeFunction extends js.Object {
-  def toBe(marbles: String): Unit
+  def toBe[T](marbles: String, values: js.Dictionary[T] = ???): Unit
 }
 
 @js.native
 @JSGlobal("Rx.TestScheduler")
-class TestScheduler(assertDeepEqual: js.Function2[Any, Any, Boolean|Unit]) extends js.Object {
-  def createHotObservable[T](marbles: T): HotObservableFacade[T] = js.native
-  def createColdObservable[T](marbles: T): ObservableFacade[T] = js.native
+class TestScheduler(assertDeepEqual: js.Function2[js.Any, js.Any, Boolean|Unit]) extends js.Object {
+  def createHotObservable[T](marbles: String, values: js.Dictionary[T] = ???): ObservableFacade[T] = js.native
+  def createColdObservable[T](marbles: String, values: js.Dictionary[T] = ???): ObservableFacade[T] = js.native
   def expectObservable[T](observable: ObservableFacade[T]): WithObservableToBeFunction = js.native
   def flush(): Unit = js.native
 }
