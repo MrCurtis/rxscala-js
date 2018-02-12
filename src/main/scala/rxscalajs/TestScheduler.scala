f51a6c1bac09f5df81c787bd70638452387cbcd5
a <4184070+MrCurtis@users.noreply.github.com>
2018-02-12 22:00:56 +0000

    Tidyup.

--- src/main/scala/rxscalajs/TestScheduler.scala
+++ src/main/scala/rxscalajs/TestScheduler.scala
@@ -4,8 +4,7 @@ import scala.scalajs.js
 import js.annotation._
 import js.|
 
-import rxscalajs.subscription.Subscription
-import rxscalajs.facade.{ObservableFacade, SubjectFacade, HotObservableFacade}
+import rxscalajs.facade.ObservableFacade
 
 
 @js.native
@@ -15,7 +14,7 @@ trait WithObservableToBeFunction extends js.Object {
 
 @js.native
 @JSImport("rxjs/Rx", "TestScheduler", globalFallback = "Rx.TestScheduler")
-class TestScheduler(assertDeepEqual: js.Function2[js.Any, js.Any, Boolean|Unit]) extends js.Object {
+class TestSchedulerFacade(assertDeepEqual: js.Function2[js.Any, js.Any, Boolean|Unit]) extends js.Object {
   def createHotObservable[T](marbles: String, values: js.Dictionary[T] = ???): ObservableFacade[T] = js.native
   def createColdObservable[T](marbles: String, values: js.Dictionary[T] = ???): ObservableFacade[T] = js.native
   def expectObservable[T](observable: ObservableFacade[T]): WithObservableToBeFunction = js.native
--- src/test/scala/rxscalajs/TestSchedulerFacadeTest.scala
+++ src/test/scala/rxscalajs/TestSchedulerFacadeTest.scala
@@ -4,21 +4,18 @@ import scala.collection.mutable.Map
 
 import utest._
 
-import rxscalajs.facade.{GroupedObservableFacade, ObservableFacade, SubjectFacade}
-import rxscalajs.facade.HotObservableFacade
-
 import scala.scalajs.js
 import scala.scalajs.js.JSConverters._
 
 case class MyCase(a: Int, b: String)
 
 
-object HotObservableTest extends TestSuite {
+object TestSchedulerFacadeTest extends TestSuite {
   def tests = TestSuite {
     'FacadeTests {
       val x = MyCase(1, "two")
       val y = MyCase(1, "two")
-      val cw = new TestScheduler((x,y) => assert(DeepEqual(x, y)))
+      val cw = new TestSchedulerFacade((x,y) => assert(DeepEqual(x, y)))
       val hotOb = cw.createHotObservable("1-2", Map("1" -> x, "2" -> x).toJSDictionary)
       val eO = cw.expectObservable(hotOb).toBe("1-2", Map("1" -> y, "2" -> y).toJSDictionary)
       cw.flush()
