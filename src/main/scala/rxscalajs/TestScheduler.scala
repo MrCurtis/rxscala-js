195a28819a4d77e771d1c8f70ff8186bba229fd9
a <4184070+MrCurtis@users.noreply.github.com>
2018-02-17 12:22:19 +0000

    Create Tescheduler (non-facade).

--- src/main/scala/rxscalajs/TestScheduler.scala
+++ src/main/scala/rxscalajs/TestScheduler.scala
@@ -1,6 +1,7 @@
 package rxscalajs
 
 import scala.scalajs.js
+import scala.scalajs.js.JSConverters._
 import js.annotation._
 import js.|
 
@@ -8,8 +9,13 @@ import rxscalajs.facade.ObservableFacade
 
 
 @js.native
-trait WithObservableToBeFunction extends js.Object {
+trait WithObservableToBeFacade extends js.Object {
   def toBe[T](marbles: String, values: js.Dictionary[T] = ???): Unit = js.native
+}
+
+class WithObservableToBeFunction(toBeFacade: WithObservableToBeFacade) {
+  def toBe[T](marbles: String, values: Map[String,T] = ???): Unit =
+    toBeFacade.toBe(marbles, values.toJSDictionary)
 }
 
 @js.native
@@ -17,7 +23,7 @@ trait WithObservableToBeFunction extends js.Object {
 class TestSchedulerFacade(assertDeepEqual: js.Function2[js.Any, js.Any, Boolean|Unit]) extends js.Object {
   def createHotObservable[T](marbles: String, values: js.Dictionary[T] = ???): ObservableFacade[T] = js.native
   def createColdObservable[T](marbles: String, values: js.Dictionary[T] = ???): ObservableFacade[T] = js.native
-  def expectObservable[T](observable: ObservableFacade[T]): WithObservableToBeFunction = js.native
+  def expectObservable[T](observable: ObservableFacade[T]): WithObservableToBeFacade = js.native
   def flush(): Unit = js.native
 }
 
@@ -25,4 +31,19 @@ class TestSchedulerFacade(assertDeepEqual: js.Function2[js.Any, js.Any, Boolean|
 @js.native
 object DeepEqual extends js.Object {
     def apply(x: js.Any, y: js.Any): Boolean = js.native
+}
+
+class TestScheduler(assertDeepEqual: js.Function2[js.Any, js.Any, Boolean|Unit]) {
+  val tsFacade = new TestSchedulerFacade((x,y) => assertDeepEqual(x,y))
+  def createHotObservable[T](marbles: String, values: Map[String, T] = ???): Observable[T] =
+    new Observable(tsFacade.createHotObservable(marbles, values.toJSDictionary))
+  def createColdObservable[T](marbles: String, values: Map[String, T] = ???): Observable[T] =
+    new Observable(tsFacade.createColdObservable(marbles, values.toJSDictionary))
+  def expectObservable[T](observable: Observable[T]): WithObservableToBeFunction =
+    new WithObservableToBeFunction(tsFacade.expectObservable(observable.inner))
+  def flush(): Unit = tsFacade.flush()
+}
+
+object TestScheduler {
+  def apply(assertDeepEqual: js.Function2[js.Any, js.Any, Boolean|Unit]) = new TestScheduler(assertDeepEqual)
 }
--- src/test/scala/rxscalajs/TestSchedulerFacadeTest.scala
+++ src/test/scala/rxscalajs/TestSchedulerFacadeTest.scala
@@ -1,7 +1,5 @@
 package rxscalajs
 
-import scala.collection.mutable.Map
-
 import utest._
 
 import scala.scalajs.js
@@ -10,15 +8,37 @@ import scala.scalajs.js.JSConverters._
 case class MyCase(a: Int, b: String)
 
 
-object TestSchedulerFacadeTest extends TestSuite {
+object TestSchedulerFacadeTests extends TestSuite {
   def tests = TestSuite {
     'FacadeTests {
       val x = MyCase(1, "two")
       val y = MyCase(1, "two")
       val cw = new TestSchedulerFacade((x,y) => assert(DeepEqual(x, y)))
       val hotOb = cw.createHotObservable("1-2", Map("1" -> x, "2" -> x).toJSDictionary)
-      val eO = cw.expectObservable(hotOb).toBe("1-2", Map("1" -> y, "2" -> y).toJSDictionary)
+      cw.expectObservable(hotOb).toBe("1-2", Map("1" -> y, "2" -> y).toJSDictionary)
       cw.flush()
+    }
+  }
+}
+
+
+object TestSchedulerTests extends TestSuite {
+  def tests = TestSuite {
+    "Create TestScheduler and use HotObservable" - {
+      val x = MyCase(1, "two")
+      val y = MyCase(1, "two")
+      val ts = TestScheduler((x,y) => assert(DeepEqual(x, y)))
+      val hotOb = ts.createHotObservable("^1-2", Map("1" -> x, "2" -> x))
+      ts.expectObservable(hotOb).toBe("-1-2", Map("1" -> y, "2" -> y))
+      ts.flush()
+    }
+    "Create TestScheduler and use ColdObservable" - {
+      val x = MyCase(1, "two")
+      val y = MyCase(1, "two")
+      val ts = TestScheduler((x,y) => assert(DeepEqual(x, y)))
+      val coldOb = ts.createColdObservable("^1-2", Map("1" -> x, "2" -> x))
+      ts.expectObservable(coldOb).toBe("-1-2", Map("1" -> y, "2" -> y))
+      ts.flush()
     }
   }
 }
